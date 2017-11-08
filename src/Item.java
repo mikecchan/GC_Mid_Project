54e29eb967821d74a9bfb0eab3ee322832180e67
Matthew Davis <xxmatt95xx@Matthews-MacBook-Air.local>
2017-11-08 16:51:47 -0500

    First commit for Item.Java

---
+++ src/Item.java
@@ -0,0 +1,46 @@
+
+public class Item {
+	String name;
+	String desc;
+	int qty;
+	String price;
+	
+	public Item(String name, String desc, int qty, String price) {
+		this.name = name;
+		this.desc = desc;
+		this.qty = qty;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
