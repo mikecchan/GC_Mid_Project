d5f951e58c4bd0db15fd5cd6d2f9040d04908558
Alex Cyr <alexcyr@Alex-Cyrs-MacBook-Pro.local>
2017-11-09 10:22:12 -0500

    added exit condition from view inventory

--- src/Shop.java
+++ src/Shop.java
@@ -19,15 +19,24 @@ public class Shop {
 		for(int i = 0; i < inventory.size(); i++) {
 			System.out.println((i+1) + "  " + inventory.get(i));
 		}
-		System.out.println((inventory.size()+1) + " Quit" );
+		System.out.println((inventory.size()+2) + " Quit" );
 		
 		int selection = getItem(sc, inventory);
+		
+		if (selection == inventory.size()+1) {
+			
+		}
+		if (selection == inventory.size()+2) {
+			
+		}
+		
 		int userQty = getQty(selection , sc, inventory);
 		
 		String addToCart = Validator.getChoice(sc, "Do you want to add to cart? (y/n): ", "yn");
 		if (addToCart.equalsIgnoreCase("n")) {
 			viewInventory( sc, inventory, cart);
 		}
+		
 		else {
 			Item tempItem = inventory.get(selection);
 			tempItem.qty = userQty;
